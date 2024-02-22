0a980b1b3a166dd3545c8978a433b0f0efacb40a
MeroCallys <MeroCallys@gmail.com>
2024-02-22 13:10:20 -0700

    Note App Starting file

--- src/app/snippets/[id]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function LoadingSnippet() {
-  return <div>loading...</div>;
-}
--- src/app/snippets/[id]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotFoundPage() {
-  return (
-    <div>
-      <h1>Sorry we couldnt find that snippet</h1>
-    </div>
-  );
-}

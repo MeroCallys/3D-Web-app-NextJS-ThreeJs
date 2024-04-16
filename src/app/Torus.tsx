5399c7c1478fa121a1e0f749ca0616bede675d30
MeroCallys <MeroCallys@gmail.com>
2024-04-15 18:01:50 -0600

    example animation

--- src/app/Torus.tsx
+++ src/app/Torus.tsx
@@ -1,27 +1,56 @@
 "use client";
 
-import { Mesh } from "three";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+
+
+import { Mesh, Vector2 } from "three";
+import { Canvas, extend, useFrame, useThree } from "@react-three/fiber";
 import React, { useState, useRef } from "react";
+import { OutlinePass } from "three/examples/jsm/Addons.js";
+
+extend({ OutlinePass });
 
 export default function Torus() {
   const [rotate, setRotation] = useState(0);
   const cubeRef = useRef<Mesh>(null!);
+  const [hovered, setHovered] = useState(true);
 
-  useFrame(({ clock }) => {
+  useFrame((state, delta) => {
     // Get elapsed time from clock
-    const elapsedTime = clock.getElapsedTime();
+    const elapsedTime = state.clock.getElapsedTime();
 
-    cubeRef.current.position.y = Math.sin(elapsedTime) * 2; // Make the cube float up and down
+    cubeRef.current.position.z = Math.random() * elapsedTime * 2; // Make the cube float up and down
+    cubeRef.current.position.x = Math.sin(elapsedTime) * 2; // Make the cube float up and down
+    cubeRef.current.position.z = Math.sin(elapsedTime) * 2; // Make the cube float up and down
     cubeRef.current.rotation.x = elapsedTime; // Rotate the cube around the X-axis
     cubeRef.current.rotation.y = elapsedTime; // Rotate the cube around the Y-axis
   });
 
   return (
     <>
-      <mesh ref={cubeRef} scale={0.5} position-x={-1}>
+      <mesh
+        ref={cubeRef}
+        scale={0.5}
+        position-x={-1}
+        onPointerEnter={(event) => {
+          event.stopPropagation();
+          setHovered(true);
+        }}
+        onClick={(event) => {
+          event.stopPropagation();
+          console.log("you clicked the spehere");
+        }}
+        onPointerLeave={(event) => {
+          event.stopPropagation();
+          setHovered(false);
+        }}
+      >
         <sphereGeometry />
-        <meshNormalMaterial wireframe />
+        {hovered ? (
+          <meshNormalMaterial wireframe />
+        ) : (
+          <meshBasicMaterial color={"purple"} wireframe />
+        )}
+        {/* <meshNormalMaterial wireframe /> */}
       </mesh>
 
       <mesh scale={0.5}>
